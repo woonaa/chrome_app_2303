f4ca44b288bfedf48f61936ea926564c29cc95a4
woon <lucky.woon.j@gmail.com>
2023-03-27 01:41:51 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("login-form buton");
+
+function onLoginBtnClick() {
+
+}
+loginButton = addEventListener("click", onLoginBtnClick);
