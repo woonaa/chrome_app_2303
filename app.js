60866c6831702ad6b1e473e5b6cbefafb8c435e6
woon <woon@jangseo.kr>
2023-03-29 16:59:26 +0900

    0329 update

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const loginForm = document.querySelector("#login-form");
-const loginInput = document.querySelector("login-form input");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-function onLoginSumit() {
-    const loginValue = loginInput.value;   
+const HIDDEN_CLASSNAME = "hidden";
+
+///preventDefalut 브라우저의 기본동작을 막아줌
+///`HEllo ${username}` 스트링 문자 합치는 팁 
+
+function onLoginSumit(event) {
+    event.preventDefault();
+    const username = loginInput.value;
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    greeting.innerText = `HEllo ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+    console.log(greeting.class);
 }
 
 loginForm.addEventListener("submit", onLoginSumit);
